dcbd4c0b2856b59b03bafcf7d26829084ff512e0
Alan Somers <asomers@gmail.com>
2022-12-21 11:17:07 -0700

    WIP on an example for how to write a Casper service.

---
+++ capsicum/examples/getuid.rs
@@ -0,0 +1,56 @@
+//! A toy Casper service that provides `getuid()`.
+
+use std::{io, ptr};
+
+use libc::uid_t;
+use casper_sys::nvlist_t;
+use capsicum::casper::{self, Casper};
+use const_cstr::{ConstCStr, const_cstr};
+use ctor::ctor;
+
+const SERVICE_NAME: ConstCStr = const_cstr!("getuid");
+
+extern fn getuid_cmd(
+    cmd: *const ::std::os::raw::c_char,
+    arg1: *const nvlist_t,
+    arg2: *mut nvlist_t,
+    arg3: *mut nvlist_t
+) -> i32 {
+    todo!()
+}
+
+// TODO: make a macro for this part
+#[ctor]
+unsafe fn init_casper_service() {
+    casper_sys::service_register(
+        SERVICE_NAME.as_cstr().as_ptr(),
+        None,
+        Some(getuid_cmd),
+        0
+    );
+}
+
+casper::service!(
+    /// A connection to the Casper `uid` helper.
+    pub CapUid, const_cstr!("getuid"), uid
+);
+
+impl CapUid {
+    pub fn uid(&mut self) -> io::Result<uid_t> {
+        let r = unsafe {
+            casper_sys::cap_xfer_nvlist(self.0.as_ptr(), ptr::null_mut())
+        };
+        if r.is_null() {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok(42)
+        }
+    }
+}
+
+fn main() {
+    let casper = Casper::new().unwrap();
+    capsicum::enter().unwrap();
+    let mut cap_uid = casper.uid().unwrap();
+    println!("{:?}", cap_uid.uid());
+}
